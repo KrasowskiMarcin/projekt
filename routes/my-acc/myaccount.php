8dbf87f1ac5564326575e8b47aaba1da46d09df1
Marcin Krasowski <mkrasowski146@technikumkreatywne.pl>
2022-11-06 11:01:10 +0100

    Front end strony my-acc

--- routes/my-acc/myaccount.php
+++ routes/my-acc/myaccount.php
@@ -1,12 +1,36 @@
+<center><h1>Moje konto</h1></center>
 <article class="container">
-    
+    <center>
+    <form action="" method="POST">
+                <p>Dane osobowe: Jan Kowalski</p>
+                <p>Nick: Jkowalski</p>
+                <p>Kontakt</p>
+                <p><textarea name="contact"></textarea></p>
+                <center><p><input type="submit" value="Zmień" /></p></center>
+    </form>
+    </center>
 </article>
 
 <style>
+center{
+    padding-top: 10px;
+}
+
 .container {
   display: flex;
-  align-items: center;
+  /* align-items: center; */
   justify-content: center;
   height: 100%;
 }
+
+input[type="submit"]{
+    width: 162px;
+    height: 41px;
+
+    background-color: #E8630A;
+    color: #fff;
+    border-radius: 15px;
+    padding: 10px;
+}
+
 </style>
