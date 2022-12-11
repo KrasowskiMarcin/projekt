594c4a42abb4fadf195d54e6988ef9ab784fb04b
Marcin Krasowski <mkrasowski146@technikumkreatywne.pl>
2022-12-11 10:28:26 +0100

    Uaktualniona baza danych

--- baza danych/szczecingadgets.sql
+++ baza danych/szczecingadgets.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 07 Lis 2022, 13:02
--- Wersja serwera: 10.4.24-MariaDB
--- Wersja PHP: 8.1.6
+-- Czas generowania: 11 Gru 2022, 10:27
+-- Wersja serwera: 10.4.22-MariaDB
+-- Wersja PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,18 +31,20 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `nazwa` varchar(255) NOT NULL,
   `opis` varchar(255) NOT NULL,
-  `url_obrazu` varchar(255) NOT NULL
+  `url_obrazu` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `products`
 --
 
-INSERT INTO `products` (`id`, `nazwa`, `opis`, `url_obrazu`) VALUES
-(3, 'Paprykarz puszka', 'Puszeczka paprykarza szczecińskiego', 'https://delikatesylosos.pl/356-medium_default/paprykarz-szczecinski-losos-ustka-170g.jpg'),
-(5, 'Szalik pogoni', 'Szaliczek pgon', 'https://sklep.pogonszczecin.pl/178-home_default/szalik-tkany-pogon.jpg'),
-(6, 'Pasztecik', 'Paszteciki szczecińskie', 'https://aryzta.pl/img/towary/1/2020_09/produkty-katalog-snaki-pasztecik-z-pieczarkami.png'),
-(7, 'Barszcz', 'barszczydło', 'https://d3iamf8ydd24h9.cloudfront.net/pictures/articles/2019/03/412124-v-1000x1000.jpg');
+INSERT INTO `products` (`id`, `nazwa`, `opis`, `url_obrazu`, `user_id`) VALUES
+(3, 'Paprykarz puszka', 'Puszeczka paprykarza szczecińskiego', 'https://delikatesylosos.pl/356-medium_default/paprykarz-szczecinski-losos-ustka-170g.jpg', NULL),
+(5, 'Szalik pogoni', 'Szaliczek pgon', 'https://sklep.pogonszczecin.pl/178-home_default/szalik-tkany-pogon.jpg', NULL),
+(7, 'Barszcz', 'barszczydło', 'https://d3iamf8ydd24h9.cloudfront.net/pictures/articles/2019/03/412124-v-1000x1000.jpg', NULL),
+(8, 'Magnes szczecin', 'Magnesy sdhasdhashsadhasdhdssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'https://pamiatkizpolski.pl/userdata/public/gfx/4933.jpg', NULL),
+(15, '23', '23232', 'https://static.bimago.pl/mediacache/catalog/product/cache/1/1/127311/image/750x1120/a8adb978a49239ecbc305ef6f8b676b8/127311_1.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -59,6 +61,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `login`, `password`, `contact`) VALUES
+(1, 'Marcin', '1', '$2y$10$1qq4L8VVuWvKurFYy9kzu.C0yJuVXM5ctIOCJ6fYdHixKhBAF4YSu', '1');
+
+--
 -- Indeksy dla zrzutów tabel
 --
 
@@ -66,7 +75,8 @@ CREATE TABLE `users` (
 -- Indeksy dla tabeli `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -82,13 +92,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
